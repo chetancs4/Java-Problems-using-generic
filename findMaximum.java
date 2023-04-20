0a60e1d85d98785d0bdc082a9633fb72063d9303
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-04-20 09:23:06 +0530

    [Chetan] Add: Added java program to find maximum string

--- findMaximum.java
+++ findMaximum.java
@@ -1,3 +1,4 @@
+
 public class Find_Maximum {
 
 	public static void toPrintMaxInteger(Integer[] inputArray) {
@@ -22,7 +23,59 @@ public class Find_Maximum {
 			}
 		}
 		System.out.println(max);
+		System.out.println("---------------------------------------------------------");
 	}
+	
+
+	public static void toPrintMaxDouble(Double[] inputArray) {
+		System.out.println("ELEMENTS IN ARRAY : ");
+		for (double element : inputArray) {
+			System.out.printf("%s", element);
+			System.out.println();
+		}
+		System.out.println();
+
+		int x;
+		double max = inputArray[0];
+		System.out.println("MAXIMUM ELEMENT IN ARRAY : ");
+		for (int i = 1; i < inputArray.length; i++) {
+			/*
+			 * Using compareTo method 
+			 * to find maximum among 
+			 * float values in array
+			 */
+			if ((inputArray[i].compareTo(max)) > 0) {
+				max = inputArray[i];
+			}
+		}
+		System.out.println(max);
+		System.out.println("---------------------------------------------------------");
+	}
+	
+
+	public static void toPrintMaxString(String[] inputArray) {
+		System.out.println("ELEMENTS IN ARRAY : ");
+		for (String element : inputArray) {
+			System.out.printf("%s", element);
+			System.out.println();
+		}
+		System.out.println();
+
+		String max = inputArray[0];
+		System.out.println("MAXIMUM ELEMENT IN ARRAY : ");
+		for (int i = 1; i < inputArray.length; i++) {
+			/*
+			 * Using compareTo method 
+			 * to find maximum among 
+			 * string values in array
+			 */
+			if ((inputArray[i].compareTo(max)) > 0) {
+				max = inputArray[i];
+			}
+		}
+		System.out.println(max);
+	}
+	
 
 	public static void main(String[] args) {
 		System.out.println();
@@ -31,8 +84,15 @@ public class Find_Maximum {
 
 		// Integer array
 		Integer[] intarr = { 10, 35, 41, 2, 6 };
-
 		Find_Maximum.toPrintMaxInteger(intarr);
+
+		// Double array
+		Double[] doublearr = { 12.4, 54.3, 64.0, 2.4, 25.7 };
+		Find_Maximum.toPrintMaxDouble(doublearr);
+
+		// String array
+		String[] stringarr = { "Apple", "Mango", "Banana", "Orange", "Pineapple" };
+		Find_Maximum.toPrintMaxString(stringarr);
 	}
 
-}s
+}
